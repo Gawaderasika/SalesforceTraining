ffeaaed18ef56311bd905674153acf4ef35354f7
Gawaderasika <67955666+Gawaderasika@users.noreply.github.com>
2020-11-10 17:44:37 +0530

    Revert "pubsub model"
    This reverts commit 33ca390059e577f53248bc1d1dc4a218c21205e2.

--- force-app/main/default/lwc/pubComp/pubComp.js
+++ force-app/main/default/lwc/pubComp/pubComp.js
@@ -2,7 +2,7 @@ import { LightningElement, wire } from 'lwc';
 import {CurrentPageReference} from 'lightning/navigation';
 import {fireEvent} from 'c/pubsub';
 
-export default class pubComp extends LightningElement {
+export default class PubComp extends LightningElement {
     @wire (CurrentPageReference) pageRef;
     callEvent(event){
         var eventParam={'firstname':'Rasika'};
